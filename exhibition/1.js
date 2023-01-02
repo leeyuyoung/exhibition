178d2c3e864164578b23174ccb75147125e2ff4a
leeyuyoung <89955240+leeyuyoung@users.noreply.github.com>
2023-01-03 02:00:54 +0900

    Add files via upload

---
+++ exhibition/1.js
@@ -0,0 +1,34 @@
+
+/*전시정보 - 팝업*/
+
+function exe(){
+    let obj = document.getElementById("popup");
+    obj.style.display="block";
+}
+function closed(){
+    document.getElementById("popup").style.display="none";
+}
+
+function exe1(){
+    let obj = document.getElementById("popup1");
+    obj.style.display="block";
+}
+function closed1(){
+    document.getElementById("popup1").style.display="none";
+}
+
+function exe2(){
+    let obj = document.getElementById("popup2");
+    obj.style.display="block";
+}
+function closed2(){
+    document.getElementById("popup2").style.display="none";
+}
+
+function exe3(){
+    let obj = document.getElementById("popup3");
+    obj.style.display="block";
+}
+function closed3(){
+    document.getElementById("popup3").style.display="none";
+}
